d155bfa62c2a7adecd42c2af154f68f1afabac98
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-04-25 14:29:43 -0400

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', [], function() {
+gulp.task('serve', function() {
+   
+    browserSync.init({
+      server: {
+          baseDir: "./src/"
+      } 
+    });
+    
+    gulp.watch("./src/**/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve'], function() {
 });
