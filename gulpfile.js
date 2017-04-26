37866c768220859ed7ad228b7e41c65175d42bde
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-04-26 12:27:20 -0400

    Add Less compilation and CDN resources

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var path = require('path');
+var less = require('gulp-less');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('serve', function() {
-   
     browserSync.init({
       server: {
           baseDir: "./src/"
@@ -10,6 +14,17 @@ gulp.task('serve', function() {
     });
     
     gulp.watch("./src/**/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('less', function() {
+    return gulp.src('./src/less/**/*.less')
+               .pipe(sourcemaps.init())
+               .pipe(less({
+                    paths: [ path.join(__dirname, 'less','src') ],
+                    plugins: [autoprefix]
+                }))
+               .pipe(sourcemaps.write())
+               .pipe(gulp.dest('./src/css'));
 });
 
 gulp.task('default', ['serve'], function() {
